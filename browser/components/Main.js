06b40854576089227510394daa74f6f761c71c94
Felicia Heiney <bhfelicia@gmail.com>
2021-08-20 19:54:18 -0400

    rendering Hello React!

---
+++ browser/components/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import axios from "axios";
+class Main extends Component {
+  render() {
+    return <h1>Hello React!</h1>;
+  }
+}
+
+export default Main;
--- browser/index.js
+++ browser/index.js
@@ -1,4 +1,7 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
-
-console.log('Hello Webpack!');
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React from "react";
+import { render } from "react-dom";
+import Main from "./components/Main";
+render(<Main />, document.getElementById("app"));
+console.log("Hello Webpack!");
